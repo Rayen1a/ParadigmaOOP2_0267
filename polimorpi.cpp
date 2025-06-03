cce7bce0eb3128818feacb42fd1cbe820620d4b6
Rayen1a <raihan01426@gmail.com>
2025-06-03 10:08:21 +0700

    membuat class anak joko

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -8,3 +8,10 @@ virtual void pesan() {
     cout<<"Pesan dari seseorang"<<endl;
 }
 };
+
+class joko :public seseorang {
+public:
+    void pesan() {
+    cout<<"Pesan dari seseorang"<<endl;
+    }
+};
